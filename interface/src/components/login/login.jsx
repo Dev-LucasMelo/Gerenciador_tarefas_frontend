92314e72540aac6a0cfb07d4d271e80f9ba74f16
Lucas Melo <lmelo072002@gmail.com>
2022-04-07 09:07:17 -0300

    responsividade login

--- interface/src/components/login/login.jsx
+++ interface/src/components/login/login.jsx
@@ -4,7 +4,7 @@ import { AiFillGithub } from 'react-icons/ai'
 
 import './login.css';
 
-function Login({user,submit}) {
+function Login({ user, submit }) {
 
   const [userlogin, setuserlogin] = useState('')
   const [userpassword, setuserpassword] = useState('')
@@ -17,51 +17,55 @@ function Login({user,submit}) {
   }
 
   //PROPS
-  user(userlogin,userpassword)
+  user(userlogin, userpassword)
 
   return (
-    <div className="login">
-      <div className="login-component">
-        <div className='form-box'>
-          <div className='Logo-tittle'>
-            <a href="https://github.com/Dev-LucasMelo" target='_blank ' ><h1 className='logo' ><AiFillGithub /></h1></a>
-            <h1 className='tittle-form' >Login</h1>
-          </div>
-          <div>
-            <div className='form'>
-              <p className='tittle-input'>Username</p>
-              <input
 
-                className='input'
-                name='login_user'
-                placeholder='Your username'
-                type="text"
-                onChange={datalogin} />
-              <p className='tittle-input'>Password</p>
-              <input
+    <div className="login-component">
 
-                className='input'
-                name='login_password'
-                placeholder='Your password'
-                type="password"
-                onChange={datapassword} />
+      <div className='form-box'>
 
-              <br></br>
-              <a className='link-cadastro' href="/cadastro">Create a new account</a>
-              <br></br>
-              <br></br>
-              <br></br>
-              <button className='submit' onClick={submit} >Login</button>
-              
-            </div>
-          </div>
+        <div className='Logo-tittle'>
+          <a href="https://github.com/Dev-LucasMelo" target='_blank ' ><h1 className='logo' ><AiFillGithub /></h1></a>
+          <h1 className='tittle-form' >Login</h1>
         </div>
-        <div className='content-login'>
-          <a href="https://github.com/Dev-LucasMelo" target="_blank " ><AiFillGithub className='github' /></a>
+
+        <div>
+          <div className='form'>
+            <p className='tittle-input'>Username</p>
+            <input
+
+              className='input'
+              name='login_user'
+              placeholder='Your username'
+              type="text"
+              onChange={datalogin} />
+            <p className='tittle-input'>Password</p>
+            <input
+
+              className='input'
+              name='login_password'
+              placeholder='Your password'
+              type="password"
+              onChange={datapassword} />
+
+            <br></br>
+            <a className='link-cadastro' href="/cadastro">Create a new account</a>
+            <br></br>
+            <br></br>
+            <button className='submit' onClick={submit} >Login</button>
+
+          </div>
         </div>
       </div>
+
+      <div className='content-login'>
+        <a href="https://github.com/Dev-LucasMelo" target="_blank " ><AiFillGithub className='github' /></a>
+      </div>
+
     </div>
   );
 }
 
 export default Login;
+
